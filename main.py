8fc4a3fa49a0084ae3183e24124b87146a4d9d36
f1zery <felik98@inbox.ru>
2025-04-15 13:35:26 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ root = Tk()
 
 # Функции приложения
 
-# def on_button_click(value):
-#     if value == "Добавить":
+def on_button_click(value):
+    selected = listbox.curselection()
+    if value == "Добавить":
+        The_finished_text = case.get()  # Получаем текст из поля ввода
+        listbox.insert(listbox.index("end"), The_finished_text)
+
+    elif value == "Удалить":
+        selected == "Удалить"
+        listbox.delete(selected[0])
+
+    elif value == "Очистить всё":
+        listbox.delete(0,listbox.index("end"))
+        
 
 # Имя окна
 root.title("Мой TODO-лист")
@@ -15,10 +26,11 @@ root.title("Мой TODO-лист")
 root.geometry("400x400")
 
 # поле ввода
-case = Entry().pack(anchor=N, padx=8, pady= 8)
+case = Entry()
+case.pack(anchor=N, padx=8, pady= 8)
 
 # добавить
-add_button = Button(text="Добавить")
+add_button = Button(root,text="Добавить", command=lambda value ="Добавить": on_button_click(value))
 add_button.pack()
 
 # cписок элементов
@@ -35,14 +47,13 @@ buttonFrame.columnconfigure(0, weight=1)
 buttonFrame.columnconfigure(1, weight=1)
 
 # удалить
-delete_button = Button(buttonFrame,text="Удалить")
+delete_button = Button(buttonFrame,text="Удалить", command=lambda value="Удалить": on_button_click(value))
 delete_button.grid(row=0, column=0, sticky="we")
 
 # очистить всё
-rab = Button(buttonFrame, text="Очистить всё") 
+rab = Button(buttonFrame, text="Очистить всё", command=lambda value="Очистить всё": on_button_click(value)) 
 rab.grid(row=0, column=1, sticky="we")
 
 # запуск приложения
 root.mainloop()
-
 
